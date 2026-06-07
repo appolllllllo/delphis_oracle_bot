3ff2f65ae67ea0d4f76336e445b4bed8c380ccfd
Tata Appolonova <appolllllllo@gmail.com>
2026-06-08 00:51:17 +0300

    add express-server for Render

--- index.js
+++ index.js
@@ -4,6 +4,19 @@ require('dotenv').config({ path: './bot_token' });
 
 const { getRandomPrediction } = require('./oracle');
 
+// Мини-веб-сервер для Render (чтобы бот не засыпал)
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+    res.send('🔮 Дельфийский Оракул жив и работает!');
+});
+
+app.listen(PORT, () => {
+    console.log(`🌐 Веб-сервер запущен на порту ${PORT}`);
+});
+
 const bot = new Telegraf(process.env.BOT_TOKEN);
 
 bot.start((ctx) => {

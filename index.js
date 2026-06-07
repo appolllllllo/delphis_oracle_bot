c2734a2950d182e65114788ac819751a5da96a00
Tata Appolonova <appolllllllo@gmail.com>
2026-06-07 23:15:42 +0300

    Added my telegram bot

---
+++ index.js
@@ -0,0 +1,23 @@
+const { Telegraf } = require('telegraf');
+
+require('dotenv').config({ path: './bot_token' });
+
+const { getRandomPrediction } = require('./oracle');
+
+const bot = new Telegraf(process.env.BOT_TOKEN);
+
+bot.start((ctx) => {
+    ctx.reply('Спрашивай, но не вини меня, если ответ тебе не понравится...')
+});
+
+bot.on('text', (ctx) => {
+    const prediction = getRandomPrediction();
+
+    ctx.reply(prediction);
+});
+
+console.log('✅ Дельфийский оракул успешно запущен!');
+bot.launch();
+
+process.once('SIGINT', () => bot.stop('SIGINT'));
+process.once('SIGTERM', () => bot.stop('SIGTERM'));
